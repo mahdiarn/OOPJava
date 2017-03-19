55ac6e09942c43a16a6e736b09660b39814dcd1c
mahdiarn <diarn_7@live.com>
2017-03-19 16:08:04 +0700

    Menambah kelas Ursidae

---
+++ src/Ursidae/Ursidae.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ursidae;
+import LandAnimal.LandAnimal;
+/**
+ *
+ * @author Mahdiar Naufal
+ */
+public class Ursidae extends LandAnimal{
+    //Constructor tanpa parameter
+    public Ursidae () {
+        weight = 0;
+        tipe = 0;
+        tamed = false;
+        location_x = -1;
+        location_y = -1;
+    }
+    
+    //Constructor dengan parameter
+    public Ursidae (int _weight, 
+            int _tipe, 
+            boolean _tamed,
+            int _x,
+            int _y) {
+        weight = _weight;
+        tipe = _tipe;
+        tamed = _tamed;
+        location_x = _x;
+        location_y = _y;
+    }
+    
+    //Copy Constructor
+    public Ursidae (Ursidae g) {
+        weight = g.weight;
+        tipe = g.tipe;
+        tamed  = g.tamed;
+        location_x = g.location_x;
+        location_y = g.location_y;
+    }
+
+    @Override
+    public void Eat() {
+        System.out.println("\"Nyam\""); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Interact() {
+        System.out.println("\"Hewan : oi!\""); //To change body of generated methods, choose Tools | Templates.
+    }
+}
