03e0322ef2ad2e0250fd0098eb9a160e662fdd7f
mahdiarn <diarn_7@live.com>
2017-03-19 21:58:01 +0700

    menambah kelas HiuPutih beserta test unitnya

---
+++ src/HiuPutih/HiuPutih.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HiuPutih;
+import Selachii.Selachii;
+import java.util.Random;
+/**
+ *
+ * @author Mahdiar Naufal
+ */
+public class HiuPutih extends Selachii{
+    //Constructor tanpa parameter
+    public HiuPutih () {
+        weight = 0;
+        tipe = 0;
+        tamed = false;
+        location_x = -1;
+        location_y = -1;
+    }
+    
+    //Constructor dengan parameter
+    public HiuPutih (int _weight, 
+            int _tipe, 
+            boolean _tamed,
+            int _x,
+            int _y) {
+        weight = _weight;
+        tipe = _tipe;
+        tamed = _tamed;
+        location_x = _x;
+        location_y = _y;
+    }
+    
+    //Copy Constructor
+    public HiuPutih (HiuPutih s) {
+        weight = s.weight;
+        tipe = s.tipe;
+        tamed  = s.tamed;
+        location_x = s.location_x;
+        location_y = s.location_y;
+    }
+
+    //Override method eat
+    @Override
+    public void Eat() {
+        System.out.println("Porsi makanan hiu putih ini adalah " + weight*0.025 + " kg."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Interact() {
+        Random rand;
+        rand = new Random();
+        
+        int n = rand.nextInt(3);
+        
+        if (n == 0){
+            System.out.println("HiuPutih : \"Zzzzzz......\"");
+        } else if (n == 1) {
+            System.out.println("HiuPutih : \"duunnn dunnn... duuuunnnn duun... duuunnnnnnnn dun dun dun dun dun dun dun dun dun dun dunnnnnnnnnnn dunnnn\"");
+        } else if (n == 2) {
+            System.out.println("HiuPutih : \"Nyam.. Nyam..\"");
+        }
+        
+    }
+}
